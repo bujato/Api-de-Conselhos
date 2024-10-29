70a36b21c8732182845eb52d66c55258106fdb0b
Enzo Guilherme Bujato <120108745+bujato@users.noreply.github.com>
2024-10-29 14:28:27 -0300

    Update index.js
    Adição de Comentários

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 let btn = document.querySelector("#btn");
 let res = document.querySelector("#res");
 
+// Utilização da API;
 fetch('https://api.adviceslip.com/advice')
 .then((advice) => advice.json())
 .then((id) => {
     res.innerHTML = id.slip.advice;
 });
 
+// Botão de Reset da página Web;
 btn.addEventListener("click", () => {
     location.reload();
 })
